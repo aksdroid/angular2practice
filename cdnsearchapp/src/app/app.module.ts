849e04c566170a76d3e45a11dc3682b89db4b63d
Amit Chudasama <amit.chudasama@gmail.com>
2017-11-15 09:55:19 +0530

    new angular2 app

--- cdnsearchapp/src/app/app.module.ts
+++ cdnsearchapp/src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
 
 
 import { AppComponent } from './app.component';
+import { SearchService } from './search.service';
 
 
 @NgModule({
@@ -10,9 +12,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,HttpModule 
   ],
-  providers: [],
+  providers: [SearchService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
